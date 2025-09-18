50c329e7639cfa126060b938654c83ef87c4f9ac
Alex Anokhin <olanokhin@gmail.com>
2025-09-18 11:31:10 +0200

    Add: sendCompositeMessage declaration;

--- src/main/kotlin/com/wire/bots/domain/message/OutgoingMessageRepository.kt
+++ src/main/kotlin/com/wire/bots/domain/message/OutgoingMessageRepository.kt
@@ -2,10 +2,17 @@ package com.wire.bots.domain.message
 
 import arrow.core.Either
 import com.wire.integrations.jvm.model.QualifiedId
+import com.wire.integrations.jvm.model.WireMessage
 
 interface OutgoingMessageRepository {
     fun sendMessage(
         conversationId: QualifiedId,
         messageContent: String
+    ): Either<Throwable, Unit>
+
+    fun sendCompositeMessage(
+        conversationId: QualifiedId,
+        messageContent: String,
+        buttonList: List<WireMessage.Button>
     ): Either<Throwable, Unit>
 }
